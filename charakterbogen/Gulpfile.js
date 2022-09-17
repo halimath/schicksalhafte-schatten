5ff0a8a2e1543fe0ab9778ba572b2f6cb63dc3e8
Alexander Metzner <alexander.metzner@gmail.com>
2022-09-17 12:53:14 +0200

    feature: neue matrixregeln
    * feature: neue matrixregeln
    
    * build: upgrade dependencies
    
    * style: kleinere korrekturen
    
    * release: 0.4.0
    
    * fix: include cyberware chapter
    
    * refactor: container-basierter build

--- charakterbogen/Gulpfile.js
+++ charakterbogen/Gulpfile.js
@@ -2,16 +2,22 @@
 
 const gulp = require("gulp");
 const Vinyl = require("vinyl");
-const sass = require("gulp-sass")(require("node-sass"));
+const sass = require("gulp-sass")(require("sass"));
 const { basename } = require("path");
 const puppeteer = require("puppeteer");
 const through = require('through2');
-const { version } = require("./package.json");
+const version = process.env.VERSION || require("./package.json").version;
 
-sass.compiler = require("node-sass");
+const footer = `
+<span style="font-family: sans-serif; font-size: 6px; width: 100%; text-align: center; color: #555555">
+    <a href="https://github.com/halimath/schwert-und-schicksal" style="text-decoration: none; color: #555555">github.com/halimath/schwert-und-schicksal</a>
+    Version ${version}. This work is licensed under a Creative Commons Attribution 4.0 License.
+</span>`
+
+sass.compiler = require("sass");
 
 function sassTask() {
-    return gulp.src("./styles/*.sass")
+    return gulp.src("./src/styles/*.sass")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("./css"));
 }
@@ -19,7 +25,7 @@ function sassTask() {
 gulp.task("sass", sassTask);
 
 gulp.task("sass:watch", function () {
-    gulp.watch(["./styles/**/*.sass"], sassTask);
+    gulp.watch(["./src/styles/**/*.sass"], sassTask);
 });
 
 function createPdf(file, enc, cb) {
@@ -27,7 +33,10 @@ function createPdf(file, enc, cb) {
     let page
     let pdfData
 
-    puppeteer.launch()
+    puppeteer.launch({
+        executablePath: '/usr/bin/chromium',
+        args: ['--no-sandbox'],
+    })
         .then(b => {
             browser = b;
             return browser.newPage();
@@ -41,12 +50,12 @@ function createPdf(file, enc, cb) {
         .then(() => page.pdf({
             format: "A4",
             printBackground: true,
-            displayHeaderFooter: true,
-            footerTemplate: `<span style="font-family: Ubuntu; font-size: 4px; width: 100%; text-align: center; color: #000; margin: 2px;"><a href="https://github.com/halimath/dnd30crs">github.com/halimath/schwert-und-schicksal</a> Version ${version}. This work is licensed under a Creative Commons Attribution 4.0 License.</span>`,
+            displayHeaderFooter: true,            
+            footerTemplate: footer,
             margin: {
                 top: 0,
                 left: 0,                
-                bottom: 0,
+                bottom: 30,
                 right: 0,
             },
         }))
@@ -62,7 +71,7 @@ function createPdf(file, enc, cb) {
 }
 
 function pdfTask () {
-    return gulp.src("./*.html")
+    return gulp.src("./src/*.html")
         .pipe(through.obj(createPdf))
         .pipe(gulp.dest("out"));
 }
@@ -70,10 +79,10 @@ function pdfTask () {
 gulp.task("pdf", pdfTask);
 
 gulp.task("pdf:watch", function () {
-    gulp.watch(["./*.html"], pdfTask);
+    gulp.watch(["./src/*.html"], pdfTask);
 });
 
 gulp.task("all", gulp.series("sass", "pdf"));
 gulp.task("all:watch", () => {
-    gulp.watch(["./*.html", "./styles/**/*.sass"], gulp.series("sass", "pdf"))
+    gulp.watch(["./src/*.html", "./src/styles/**/*.sass"], gulp.series("sass", "pdf"))
 });
